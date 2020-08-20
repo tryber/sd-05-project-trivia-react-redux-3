e2610ce7173b00d620bdccf7a2111f17b5b42372
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-20 00:53:49 -0300

    add header da tela do jogo

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getProfilePicture } from '../services/Api';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      image: '',
+    }
+  }
+  componentDidMount() {
+    const { email } = this.props.user;
+    getProfilePicture(email)
+      .then((response => this.setState({ image: response })));
+  }
+
   render() {
+    const { name, email } = this.props.user;
     return (
       <div>
-        <h2>Tela do Jogo</h2>
+        <header>
+          <img
+            data-testid="header-profile-picture"
+            alt="imagem do usuário"
+            src={this.state.image}
+          />
+          <p data-testid="header-player-name">{name}</p>
+          <p>{email}</p>
+          <p data-testid="header-score">0</p>
+        </header>
       </div>
     );
   }
@@ -13,6 +36,7 @@ class Game extends React.Component {
 
 const mapStateToProps = (state) => ({
   token: state.getToken.token,
+  user: state.loginReducer.user,
 });
 
 export default connect(mapStateToProps)(Game);
