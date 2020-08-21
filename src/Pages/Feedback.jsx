27c4d270990f409ea2051b19fae1853a6aec3839
Lucas Costa Ventura <lucascv333@gmail.com>
2020-08-21 01:25:16 -0300

    adicionando botao jogar novamente

--- src/Pages/Feedback.jsx
+++ src/Pages/Feedback.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Header from '../components/Header';
 
+import { Button } from '../components/Inputs';
+
 class Feedback extends React.Component {
+  backToHome(ev) {
+    ev.preventDefault();
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div className="container-header">
         <header>
           <Header />
+          <Button
+            data-testid="btn-play-again"
+            onClick={(ev) => { this.backToHome(ev); }}
+          >
+            Jogar novamente
+          </Button>
         </header>
       </div>
     );
