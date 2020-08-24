ee8a2f1b641b0f9ae35f579efe1e17f58e29cb74
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-24 15:45:51 -0300

    feedback message assertions

--- src/Pages/Feedback.jsx
+++ src/Pages/Feedback.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-
+import { loadPlayerLocalStorage } from '../services/localStorage';
 import { Button } from '../components/Inputs';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      assertions: 0,
+    };
+  }
+
+  componentDidMount() {
+    const state = loadPlayerLocalStorage.assertions
+    this.setState({ assertions: {state} });
+  }
+
   backToHome(ev) {
     ev.preventDefault();
     const { history } = this.props;
@@ -23,16 +35,25 @@ class Feedback extends React.Component {
         <header>
           <Header />
         </header>
+        {this.state >= 3 ? (
+          <h3 data-testid="feedback-text">Mandou bem!</h3>
+        ) : (
+          <h3 data-testid="feedback-text">Podia ser melhor...</h3>
+        )}
         <Button
           testId="btn-play-again"
-          onClick={(ev) => { this.backToHome(ev); }}
+          onClick={(ev) => {
+            this.backToHome(ev);
+          }}
         >
           Jogar novamente
         </Button>
         <Button
           type="button"
           testId="btn-ranking"
-          onClick={(ev) => { this.goToRanking(ev); }}
+          onClick={(ev) => {
+            this.goToRanking(ev);
+          }}
         >
           Ver Ranking
         </Button>
@@ -43,6 +64,7 @@ class Feedback extends React.Component {
 
 Feedback.propTypes = {
   history: PropTypes.instanceOf(Object).isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
 export default Feedback;
